27c90486da28a782e25524862b120130b6eb5925
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-04-14 23:25:48 +0530

    Added filter Component and get filter data from child to parent

---
+++ Module2/expense-tracker/src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ExpenseFilter.css'
+const ExpenseFilter =(props)=>{
+    const filterChangeHandler =(event)=>{
+        //console.log(event.target.value);
+        props.getSelectYear(event.target.value);
+    }
+    return(
+        <div className='expenses-filter'>
+            <div className='expenses-filter__control'>
+                <label>Filter By Year</label>
+                <select onChange={filterChangeHandler}>
+                    <option value='2020'>2020</option>
+                    <option value='2021'>2021</option>
+                    <option value='2022'>2022</option>
+                    <option value='2023'>2023</option>
+                    <option value='2024'>2024</option>
+                    <option value='2025'>2025</option>
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseFilter;
--- Module2/expense-tracker/src/components/Expenses/Expenses.js
+++ Module2/expense-tracker/src/components/Expenses/Expenses.js
@@ -1,8 +1,13 @@
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css';
+import ExpenseFilter from './ExpenseFilter';
 const Expenses=(props)=>{
+    const filterChangeHandler=(filterData)=>{
+        console.log(filterData);
+    }
     return (
         <div className='expenses'>
+            <ExpenseFilter getSelectYear={filterChangeHandler}></ExpenseFilter>
             {
                  props.item.map((p)=><ExpenseItem
                  title={p.title}
