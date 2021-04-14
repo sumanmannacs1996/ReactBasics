a8a47bf50a770edf0ac47d79ca10dd2b16af2e3c
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-04-14 21:53:38 +0530

    Code added for Form submition and two way buinding

--- Module2/expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ Module2/expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './ExpenseForm.css';
 const ExpenseForm =(props)=>{
+
+    const[userInput,setUserInput] =useState({
+        title:'',
+         amount:'',
+         date:''
+    });
+    const titleChangeHadler =(event)=>{
+        setUserInput((prevState)=>{
+            return {...prevState,title:event.target.value};
+        })
+     //   console.log(userInput);
+    }
+    const amountChangeHadler =(event)=>{
+        setUserInput((prevState)=>{
+            return{...prevState,amount:event.target.value}
+        })
+     //   console.log(userInput);
+    }
+    const dateChangeHadler =(event)=>{
+        setUserInput((prevState)=>{
+            return{...prevState,date:event.target.value}
+        })
+     //   console.log(userInput);
+    }
+
+    const submitHandler =(event)=>{
+        event.preventDefault();
+        console.log(userInput);
+        setUserInput({
+            title:'',
+         amount:'',
+         date:''
+        })
+    }
+
     return(
         <form>
             <div className='new-expense__controls'>
                 <div className ='new-expense__control'>
                     <label>Title</label>
-                    <input type='text'></input>
+                    <input type='text' onChange={titleChangeHadler} value ={userInput.title}></input>
                 </div>
                 <div className ='new-expense__control'>
                     <label>Amount</label>
-                    <input type ='number' min='0.01' step='0.01'></input>
+                    <input type ='number' min='0.01' step='0.01' onChange={amountChangeHadler} value={userInput.amount}></input>
                 </div>
                 <div className ='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2020-01-01' max='2025-12-31'></input>
+                    <input type='date' min='2020-01-01' max='2025-12-31' onChange={dateChangeHadler} value={userInput.date}></input>
                 </div>
             </div>
             <div className ='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
+                <button type='submit' onClick={submitHandler}>Add Expense</button>
             </div>
         </form>
     );
