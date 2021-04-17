02773d3344ae6f6be4c7279a0781f67a756d705a
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-04-17 19:37:27 +0530

    added dynamic inline css by style={{}}

--- Module3/course-list/src/components/Course/CourseInput.js
+++ Module3/course-list/src/components/Course/CourseInput.js
@@ -3,11 +3,14 @@ import Button from '../UI/Button/Button'
 import './CouseInput.css'
 const CourseInput = (props)=>{
     const [entiredValue,setEnterdValue]=useState('');
+    const [isValid,setState] = useState(true);
     const nameChangeHandler =(event)=>{
         setEnterdValue(event.target.value);
+        setState(true);
     }
     const addCouseHandler=()=>{
         if(entiredValue.trim().length === 0){
+            setState(false);
             return;
         }
         props.add(entiredValue);
@@ -15,8 +18,11 @@ const CourseInput = (props)=>{
     return(
         <form>
             <div className="form-control">
-                <label>Course Name</label>
-                <input type='text' onChange={nameChangeHandler}></input>
+                <label style={{color: !isValid ? 'red':'black'}}>Course Name</label>
+                <input type='text' onChange={nameChangeHandler} 
+                style={{borderColor: !isValid ? 'red' :'#ccc',
+                        backgroundColor: !isValid ? 'salmon': 'transparent'
+                }}></input>
             </div>
             <Button type='button' click={addCouseHandler}>Add Course</Button>
         </form>
