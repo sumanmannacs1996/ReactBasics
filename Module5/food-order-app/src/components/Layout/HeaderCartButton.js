a625e09d4ab417228aa05c741110f398ea3f1d26
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-04-24 21:14:22 +0530

    added animation once cart updated cart button bump up

--- Module5/food-order-app/src/components/Layout/HeaderCartButton.js
+++ Module5/food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,27 @@
-import React,{useContext} from 'react'
+import React,{useContext, useEffect,useState} from 'react'
 import CartIcon from '../Cart/CartIcon'
 import styles from './HeaderCartButton.module.css'
 import cartContect from '../../store/cart-context'
 export default function HeaderCartButton(props) {
+    const [isButtonHighlited,setButtonHighlited] = useState(false);
     const cartCtx = useContext(cartContect);
-    const numberOfCartItem = cartCtx.items.reduce((cNum,p)=>{return cNum+p.quantity},0);
+    const {items} = cartCtx;
+    const numberOfCartItem = items.reduce((cNum,p)=>{return cNum+p.quantity},0);
+    const btnClas = `${styles.button} ${isButtonHighlited ? styles.bump : ''}`;
+    useEffect(()=>{
+       if(items.length === 0)
+            return;
+        setButtonHighlited(true);
+        const timer =setTimeout(()=>{
+            setButtonHighlited(false);
+        },300);
+    return ()=>{
+        clearTimeout(timer);
+    }    
+    },[items]);
     return (
         <div>
-            <button className={styles.button} onClick={props.clicked}>
+            <button className={btnClas} onClick={props.clicked}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
