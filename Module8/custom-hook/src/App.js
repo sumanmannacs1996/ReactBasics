f64032824b6409a6ff3f2971bc45adb5399ba1c3
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-04-28 03:26:17 +0530

    Created a forword and backword counter using useEffect and useState

---
+++ Module8/custom-hook/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Forword from './components/Forword';
+import Backword from './components/Backword';
+function App() {
+  return (
+    <div>
+     <Forword></Forword>
+     <Backword></Backword>
+    </div>
+  );
+}
+
+export default App;
---
+++ Module8/custom-hook/src/components/Forword.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react'
+import Card from './Card'
+export default function Forword() {
+    const [counter,setCounter]=useState(0);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setCounter((preState)=>preState +1);
+        },1000)
+    })
+
+    return <Card>{counter}</Card>
+}
