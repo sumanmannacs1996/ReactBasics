4cae08e46327aa94e928b24eb992d16e35794d77
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-05-01 20:20:40 +0530

    added code to fetch meals from firebase

--- Module5/food-order-app/src/components/Meals/AvailableMeals.js
+++ Module5/food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,38 +1,58 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import styles from './AvailableMeals.module.css'
 import Card from '../UI/Card'
 import MealItem from './MealItem/MealItem'
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+
 export default function AvailableMeals() {
+  const [mealsData,setMealsData] = useState([]);
+  const [isLoading,setLoading] = useState(true);
+  const [error,setError] = useState(undefined);
+  useEffect(()=>{
+    const fetchMeals = async ()=>{
+      try{
+        const res = await fetch('https://react-http-1e282-default-rtdb.firebaseio.com/meals.json');
+        if(!res.ok){
+          throw new Error('Not able to fetch :(');
+        }
+        const data = await res.json();
+        const MealsData =[];
+        for(const key in data){
+          MealsData.push({
+            id:key,
+            name:data[key].name,
+            description:data[key].description,
+            price:data[key].price,
+          });
+        }
+        setMealsData(MealsData);
+        setLoading(false);
+      }catch(error){
+        setLoading(false);
+        setError(error.message);
+      }
+    }
+    fetchMeals();
+  },[]);
+
+  if(isLoading){
+    return(
+      <section className={styles.loading}>
+        <p>Loading Meals For You...</p>
+      </section>
+    );
+  }
+  if(error){
+    return(
+      <section className={styles.error}>
+        <p>{error}</p>
+      </section>
+    );
+  }
+
     return (
         <div className={styles.meals}>
             <Card >
-                {DUMMY_MEALS.map((p)=><MealItem data={p} key={p.id}></MealItem>)}
+                {mealsData.map((p)=><MealItem data={p} key={p.id}></MealItem>)}
             </Card>
         </div>
     )
