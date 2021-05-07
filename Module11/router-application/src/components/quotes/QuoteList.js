4438c21b1a7b76a4307682e5864c1c634a5519c1
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-05-07 23:38:53 +0530

    code for sorting components assesnding or desending order

--- Module11/router-application/src/components/quotes/QuoteList.js
+++ Module11/router-application/src/components/quotes/QuoteList.js
@@ -1,13 +1,40 @@
 import { Fragment } from 'react';
-
+import {useHistory, useLocation} from 'react-router-dom'
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+const sortQuotes =(quotes,assending)=>{
+  return quotes.sort((a,b)=>{
+    if(assending){
+      return a>b ? 1:-1;
+    }
+    else{
+      return a<b ?1:-1;
+    }
+  });
+}
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParam = new URLSearchParams(location.search);
+
+  const isSortingAssending = queryParam.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes,isSortingAssending);
+  
+  const changeSortingHandler=()=>{
+    history.push(`/quotes?sort=${isSortingAssending ?'desc' : 'asc'}`);
+  }
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort {isSortingAssending ? 'Descending' : 'Ascending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
